c171bea1ac545e28e4ed9f884706e98cb1debd4b
Nayab Samar <nayab.s@agrya.in>
2018-09-24 10:50:30 +0530

    ui

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,9 +1,8 @@
 class InvoicesController < ApplicationController
 
-  before_action :set_invoices
 
   def index
-
+    @invoices = Invoice.all.page(params[:page]).per(15)
   end
 
   def new
@@ -25,12 +24,14 @@ class InvoicesController < ApplicationController
     # binding.pry
     invoices_with_due = ActiveRecord::Base.connection.execute("select * from (select inc.id, inc.amount, inc.reference, co.invoice_id as co_id, co.amount as co_amnt, co.reference as co_ref,(inc.amount - co.amount) / 2 as due from collections co, invoices inc where co.invoice_id = inc.id) z where due > 0;")
     @invoices = (invoices_with_due.to_a)
+    @invoices = Kaminari.paginate_array(@invoices).page(params[:page]).per(15)
   end
 
   def collected
     # @invoices = Invoice.includes(:collections).where("amount < ?", 0).page(2)
     invoices_with_due = ActiveRecord::Base.connection.execute("select * from (select inc.id, inc.amount, inc.reference, co.invoice_id as co_id, co.amount as co_amnt, co.reference as co_ref,(inc.amount - co.amount) / 2 as due from collections co, invoices inc where co.invoice_id = inc.id) z where due = 0;")
     @invoices = (invoices_with_due.to_a)
+    @invoices = Kaminari.paginate_array(@invoices).page(params[:page]).per(15)
   end
 
   private
@@ -38,9 +39,5 @@ class InvoicesController < ApplicationController
   def invoice_params
     params.require(:invoice).permit(:brand_manager, :narration, :invoice_date,
                                     :amount, :customer_name, :reference)
-  end
-
-  def set_invoices
-    @invoices = Invoice.all.page(10)
   end
 end
